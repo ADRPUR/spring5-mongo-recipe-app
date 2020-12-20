61ffc1af13ab75dc2221b18ac8e81f7d461d1fc9
ADRPUR <apurice@gmail.com>
2020-12-20 13:12:35 +0200

    configure mongodb

--- src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
+++ src/main/java/guru/springframework/converters/IngredientToIngredientCommand.java
@@ -6,10 +6,6 @@ import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
-
-/**
- * Created by jt on 6/21/17.
- */
 @Component
 public class IngredientToIngredientCommand implements Converter<Ingredient, IngredientCommand> {
 
@@ -29,9 +25,7 @@ public class IngredientToIngredientCommand implements Converter<Ingredient, Ingr
 
         IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
-        if (ingredient.getRecipe() != null) {
-            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
-        }
+
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUom(uomConverter.convert(ingredient.getUom()));
